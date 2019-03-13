806ccff27ae4e85438b00f7871f43317e51c2495
fedor-bystrov <augustbystrov@gmail.com>
2019-03-13 22:49:43 +0300

    clean up TranCheckApplication

--- src/main/java/com/example/trancheck/TranCheckApplication.java
+++ src/main/java/com/example/trancheck/TranCheckApplication.java
@@ -1,13 +1,24 @@
 package com.example.trancheck;
 
+import com.example.trancheck.controller.CsvTransactionController;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TranCheckApplication {
+public class TranCheckApplication implements CommandLineRunner {
+  private CsvTransactionController csvTransactionController;
+
+  public TranCheckApplication(CsvTransactionController csvTransactionController) {
+    this.csvTransactionController = csvTransactionController;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(TranCheckApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    csvTransactionController.checkTransactions();
+  }
 }
