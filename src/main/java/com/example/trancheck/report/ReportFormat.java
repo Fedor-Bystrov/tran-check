e61165da19e7e5bdc019a67bef9c245b5c7aac57
fedor-bystrov <augustbystrov@gmail.com>
2019-03-14 18:27:34 +0300

    add javadoc to ReportFormat

--- src/main/java/com/example/trancheck/report/ReportFormat.java
+++ src/main/java/com/example/trancheck/report/ReportFormat.java
@@ -1,5 +1,9 @@
 package com.example.trancheck.report;
 
+/**
+ * ReportFormat определяет формат файла с отчетом о сверке транзакций
+ * см. {@link ReportGenerator}
+ */
 public enum ReportFormat {
   SIMPLE_CSV_REPORT,
 }
