083ab9141c559d8b6ff31cfde4969cbbea1356b8
fedor-bystrov <augustbystrov@gmail.com>
2019-03-14 14:08:40 +0300

    use NamedParameterJdbcTemplate in TransactionRepository

--- src/main/java/com/example/trancheck/repository/TransactionRepository.java
+++ src/main/java/com/example/trancheck/repository/TransactionRepository.java
@@ -3,26 +3,31 @@ package com.example.trancheck.repository;
 import com.example.trancheck.entity.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class TransactionRepository {
   private static final Logger LOGGER = LoggerFactory.getLogger(TransactionRepository.class);
 
-  private final JdbcTemplate jdbcTemplate;
+  private final NamedParameterJdbcTemplate jdbcTemplate;
 
-  public TransactionRepository(JdbcTemplate jdbcTemplate) {
+  public TransactionRepository(NamedParameterJdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
   }
 
-  public List<Transaction> getTransactions() {
+  public List<Transaction> getTransactions(Set<Integer> transactionIds) {
     LOGGER.info("Fetching transactions from database");
-    return jdbcTemplate.query("SELECT id, amount, data FROM transactions", this::mapRow);
+    return jdbcTemplate.query(
+      "SELECT id, amount, data FROM transactions WHERE id IN (:ids)",
+      new MapSqlParameterSource("ids", transactionIds),
+      this::mapRow);
   }
 
   private Transaction mapRow(ResultSet resultSet, int rowNum) throws SQLException {
